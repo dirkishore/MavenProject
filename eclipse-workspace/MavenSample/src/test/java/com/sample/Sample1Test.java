c6aa0581f894c0fd53d3ffed478ebe09e18a6155
root <root@WhiteDevil>
2022-06-20 01:27:14 +0530

    update

---
+++ eclipse-workspace/MavenSample/src/test/java/com/sample/Sample1Test.java
@@ -0,0 +1,17 @@
+package com.sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Sample1Test {
+	@Test
+	public void sample() {
+		System.setProperty("webdriver.gecko.driver", "/root/Downloads/geckodriver");
+		
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("https://www.youtube.com/");
+		
+	}
+}
