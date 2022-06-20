7ffa4fbb227e98a8dfd5a095812e7e2015b5cc3d
root <root@WhiteDevil>
2022-06-20 14:00:26 +0530

    updated code

--- eclipse-workspace/MavenSample/src/test/java/com/sample/Sample1Test.java
+++ eclipse-workspace/MavenSample/src/test/java/com/sample/Sample1Test.java
@@ -1,5 +1,7 @@
 package com.sample;
 
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
