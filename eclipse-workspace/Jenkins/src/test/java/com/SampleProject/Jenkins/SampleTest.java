9b3db30be999b4d8c451a0005afdb40a06534fb1
root <root@WhiteDevil>
2022-06-22 14:24:29 +0530

    add code

---
+++ eclipse-workspace/Jenkins/src/test/java/com/SampleProject/Jenkins/SampleTest.java
@@ -0,0 +1,29 @@
+package com.SampleProject.Jenkins;
+
+import org.testng.annotations.Test;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SampleTest {
+
+	@Test
+	public void login() throws InterruptedException {
+
+		System.setProperty("webdriver.gecko.driver", "/root/Downloads/geckodriver");
+
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("https://www.google.com/");
+
+		driver.navigate().to("https://www.amazon.in/");
+
+		driver.navigate().back();
+
+		Thread.sleep(5000);
+
+		driver.close();
+
+	}
+
+}
